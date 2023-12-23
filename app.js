8fd33103f0ee5ecab3c037ebb437118d544cfa9a
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-12-23 21:52:50 +0100

    selecting color piker value

--- app.js
+++ app.js
@@ -2,29 +2,31 @@
 function makeGrid(row, cel) {
   for (i = 0; i < row; i++) {
     const newRow = document.createElement("tr");
-    // newRow.textContent = `linha ${i}`;
     const newTable = document.querySelector("#pixelCanvas");
     newTable.appendChild(newRow);
-    console.log(`linha ${i}`);
     for (j = 0; j < cel; j++) {
       const newCell = document.createElement("td");
       const tableRow = document.getElementsByTagName("tr")[i];
       tableRow.appendChild(newCell);
-      console.log(`linha ${i}, cel ${j}`);
     }
   }
 }
+
+const colorPiker = document.getElementById("colorPicker");
+let cor = "000000";
+colorPiker.addEventListener("change", function (event) {
+  cor = colorPiker.value;
+  console.log(`cor ${cor}`);
+});
 
 //adding event delegation to <table> element
 const newTable = document.querySelector("table");
 newTable.addEventListener("click", function (event) {
   const targetCell = event.target;
-  targetCell.style.cssText = "background-color:blue";
-  console.log("uma celula foi clicada");
+  targetCell.style.cssText = `background-color:${cor}`;
 });
 
 const submitButton = document.getElementById("sizePicker");
-
 submitButton.addEventListener("submit", function (event) {
   event.preventDefault();
   let row = document.getElementById("inputHeight").value;
