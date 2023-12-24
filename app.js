8ff64835caf9e220f980539780b5be088f8a8d00
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-12-24 18:57:32 +0100

    fixing reset table problem

--- app.js
+++ app.js
@@ -1,36 +1,49 @@
-// Creating table
+// Creating grid using user interaction
+
 function makeGrid(row, cel) {
+  const table = document.getElementById("pixelCanvas");
+  table.innerHTML = "";
+
   for (i = 0; i < row; i++) {
     const newRow = document.createElement("tr");
-    const newTable = document.querySelector("#pixelCanvas");
-    newTable.appendChild(newRow);
+    table.appendChild(newRow);
     for (j = 0; j < cel; j++) {
       const newCell = document.createElement("td");
       const tableRow = document.getElementsByTagName("tr")[i];
       tableRow.appendChild(newCell);
+
+      newCell.addEventListener("click", function (event) {
+        const colorPiker = document.getElementById("colorPicker");
+        const targetCell = event.target;
+        targetCell.style.cssText = `background-color:${colorPiker.value}`;
+      });
     }
   }
 }
 
-const colorPiker = document.getElementById("colorPicker");
-let cor = "000000";
-colorPiker.addEventListener("change", function (event) {
-  cor = colorPiker.value;
-  console.log(`cor ${cor}`);
-});
+//selecting color picker value change
+// const colorPiker = document.getElementById("colorPicker");
+// let cor = colorPiker.value;
+// colorPiker.addEventListener("change", function (event) {
+//   cor = colorPiker.value;
+// });
 
-//adding event delegation to <table> element
-const newTable = document.querySelector("table");
-newTable.addEventListener("click", function (event) {
-  const targetCell = event.target;
-  targetCell.style.cssText = `background-color:${cor}`;
-});
+//changing cell pixel grid colors
+// document.querySelector("table").addEventListener("click", function (event) {
+//   const colorPiker = document.getElementById("colorPicker");
+//   const targetCell = event.target;
+//   targetCell.style.cssText = `background-color:${colorPiker.value}`;
+// });
 
-const submitButton = document.getElementById("sizePicker");
-submitButton.addEventListener("submit", function (event) {
-  event.preventDefault();
-  let row = document.getElementById("inputHeight").value;
-  let cel = document.getElementById("inputWidth").value;
+// selecting pixel grid size values and call makeGrid function
+document
+  .getElementById("sizePicker")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    let row = document.getElementById("inputHeight").value;
+    let cel = document.getElementById("inputWidth").value;
+    makeGrid(row, cel);
+  });
 
-  makeGrid(row, cel);
-});
+// const table = document.getElementById("pixelCanvas");
+// table.removeChild("tr");
